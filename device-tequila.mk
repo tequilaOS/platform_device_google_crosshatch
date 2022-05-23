8179ea450292f6bd414cfdcc339f11536f5ba1a8
John Galt <johngaltfirstrun@gmail.com>
2022-05-23 10:37:45 -0400

    b1c1: ux: Make dex2oat go fast
    Make ota updater go a bit faster.
    
    Change-Id: I3159a0d16d8ce9c6fcb3a138384f2e2f47200a57

--- device-tequila.mk
+++ device-tequila.mk
@@ -19,6 +19,13 @@ PRODUCT_PRODUCT_PROPERTIES += \
 # Elmyra
 PRODUCT_PACKAGES += \
     ElmyraService
+    
+# Dex to make OTA go faster
+PRODUCT_VENDOR_PROPERTIES += \
+    dalvik.vm.boot-dex2oat-cpu-set=1,2,3,4,5,6,7 \
+    dalvik.vm.boot-dex2oat-threads=7 \
+    dalvik.vm.image-dex2oat-cpu-set=0,1,2,3,4,5,6,7 \
+    dalvik.vm.image-dex2oat-threads=8
 
 # EUICC
 PRODUCT_COPY_FILES += \
